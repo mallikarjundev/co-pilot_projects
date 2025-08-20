1a75fd2de288f104b8feba504eeb2140e95a11dc
arjunoggu <mallikarjundev2@gmail.com>
2025-08-19 23:39:07 -0400

    Add Main test runner to verify StockPriceService mock

--- stock-aggregator/src/main/java/com/arjun/stock/Application.java
+++ stock-aggregator/src/main/java/com/arjun/stock/Application.java
@@ -1,5 +1,9 @@
 package com.arjun.stock;
 
+import com.arjun.stock.model.StockPrice;
+import com.arjun.stock.service.StockPriceService;
+import com.arjun.stock.service.impl.StockPriceServiceImpl;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,15 @@ public class Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        StockPriceService stockPriceService = new StockPriceServiceImpl();
+
+        // Test with a few sample stock symbols
+        StockPrice apple = stockPriceService.getStockPrice("AAPL");
+        StockPrice google = stockPriceService.getStockPrice("GOOGL");
+        StockPrice amazon = stockPriceService.getStockPrice("AMZN");
+
+        System.out.println(apple);
+        System.out.println(google);
+        System.out.println(amazon);
     }
 }
